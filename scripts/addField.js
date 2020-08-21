30cc5bc04b930a9257c0babb5d8f03408c4060f0
PedroAugusto <nerdindiano@gmail.com>
2020-08-20 21:39:44 -0300

    add script to create a new container when click to a button

---
+++ scripts/addField.js
@@ -0,0 +1,18 @@
+// Procurar o botao
+document.querySelector("#add-time")
+  // Quando clicar no botao
+  .addEventListener('click', cloneField);
+// Executar uma acao
+function cloneField() {
+  // Duplicar os campos
+  const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+  // pegar os campos, que campos?
+  const fields = newFieldContainer.querySelectorAll("input")
+  // para cada campo, limpar
+  fields.forEach(function (field) {
+    field.value = ""
+  })
+  // Colocar na página, onde??
+  document.querySelector("#schedule-items").appendChild(newFieldContainer)
+}
+
